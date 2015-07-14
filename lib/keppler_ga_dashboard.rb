086f62e858637ae2a334040307d2773de4dca34d
Gabriel <gabriel@softwarecriollo.com>
2015-07-14 15:56:26 -0430

    update

--- lib/keppler_ga_dashboard.rb
+++ lib/keppler_ga_dashboard.rb
@@ -1,7 +1,7 @@
 require "keppler_ga_dashboard/engine"
 
 module KepplerGaDashboard
-  ROUTE_SIDEBAR = { controller: 'dashboard', route: '/admin/dashboard', icon: 'dashboard', title: "Dashboard" }
+  ROUTE_SIDEBAR = true
   # Default way to setup ContactUs. Run rake contact_us:install to create
   # a fresh initializer with all configuration values.
   def self.setup
